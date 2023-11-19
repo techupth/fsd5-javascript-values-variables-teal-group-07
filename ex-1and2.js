14898816ae0be0d1980209e91bce1a2c3f5f9c88
suphattraying <suphattra.spk@gmail.com>
2023-11-19 13:45:52 +0700

    feat:finnish

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstname = "Karter";
+let lastname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+console.log(address)
+
+const restaurant_name ="Chef's Table by Chef James"
+const total_price =15000;
+const shop_opened = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address ="(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+console.log(address)
+total_price=5000;
+shop_opened = 2500;
